b762c25c1d9788c58a1c3ddfce07cb3cc90e0d0e
Ubuntu <ubuntu@ip-172-31-90-152.ec2.internal>
2022-02-17 02:02:52 +0000

    Created HelloWorld.java.

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+* The HelloWorld program implements an application that
+* simply displays "Hello World!" to the standard output.
+*
+* @author  InaTolo
+* @version 1.0
+* @since   2022-2-16
+*/
+
+class HelloWorld {
+    /**
+     * Displays text
+     * to the terminal.
+     *
+     * @param args nothing passed in
+     */
+    private static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
