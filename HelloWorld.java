570b3ea92d5b815818687dd2b4ecf5e75170f9d2
Ina Tolo <94471679+InaTolo@users.noreply.github.com>
2022-02-20 10:22:46 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -14,7 +14,7 @@ class HelloWorld {
      *
      * @param args nothing passed in
      */
-    private static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 }
